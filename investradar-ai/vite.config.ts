ce0b9ce5f088feee40073edaafce7296ab6623e4
vdsbandeira-collab <vdsbandeira@gmail.com>
2025-11-23 21:09:39 -0300

    Update vite.config.ts

--- investradar-ai/vite.config.ts
+++ investradar-ai/vite.config.ts
@@ -3,21 +3,26 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    // --- ADICIONE ESTA LINHA AQUI ---
+    // Se o nome do seu repositório no GitHub não for "investradar-ai", altere aqui.
+    base: "/investradar-ai/", 
+    // --------------------------------
+    
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
